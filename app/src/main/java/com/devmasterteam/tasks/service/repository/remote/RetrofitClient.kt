b975f52fdedb7357a61d98ac46d8c340a373ee15
mateus dias <mateusdias.her@gmail.com>
2024-12-23 09:54:17 -0300

    implementação do fluxo de login, apos a implementação do retrofit

--- app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
@@ -25,5 +25,9 @@ class RetrofitClient private constructor() {
 
             return INSTANCE
         }
+
+        fun <T> getService(serviceClass: Class<T>): T {
+            return getRetrofitInstance().create(serviceClass)
+        }
     }
 }
