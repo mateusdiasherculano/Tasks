b1dd1beabff8ebb3dd362f8d91a0bec53d4d6c24
mateus dias <mateusdias.her@gmail.com>
2025-01-08 21:40:48 -0300

    ajustes finos na classe repository, retirei todos os hashcodes nessa classe

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
@@ -1,14 +1,18 @@
 package com.devmasterteam.tasks.service.repository
 
+import android.content.Context
+import com.devmasterteam.tasks.R
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.repository.remote.PersonService
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PersonRepository {
+class PersonRepository(val context: Context) {
 
    private val remote = RetrofitClient.getService(PersonService::class.java)
 
@@ -17,18 +21,19 @@ class PersonRepository {
       call.enqueue(object : Callback<PersonModel>{
          override fun onResponse(call: Call<PersonModel>, response: Response<PersonModel>) {
 
-            if (response.code() == 200){
+            if (response.code() == TaskConstants.HTTP.SUCCESS){
                response.body()?.let { listener.onSuccess(it) }
             }else{
-               // TODO - Tratar JSON
-               listener.onFailure(response.errorBody()!!.string())
+               listener.onFailure(failResponse(response.errorBody()!!.string()))
             }
          }
 
          override fun onFailure(call: Call<PersonModel>, t: Throwable) {
-            listener.onFailure("Error inesperado.")
+            listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
          }
       })
-
+   }
+   private fun failResponse(str: String): String {
+      return Gson().fromJson(str, String::class.java)
    }
 }
