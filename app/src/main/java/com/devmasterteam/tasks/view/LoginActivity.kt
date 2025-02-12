fb9398b206c772154e9748061e20a5c3ac347dff
mateus dias <mateusdias.her@gmail.com>
2025-02-12 19:58:25 -0300

    Criando a funcionalidade onde verifica se o usuario esta logado

--- app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
@@ -29,28 +29,37 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonLogin.setOnClickListener(this)
         binding.textRegister.setOnClickListener(this)
 
+        viewModel.verifyLoggedUser()
+
         // Observadores
         observe()
     }
 
     override fun onClick(v: View) {
-        if(v.id == R.id.button_login){
+        if (v.id == R.id.button_login) {
             handleLogin()
         }
     }
 
     private fun observe() {
         viewModel.login.observe(this) {
-            if(it.status()){
+            if (it.status()) {
                 startActivity(Intent(applicationContext, MainActivity::class.java))
                 finish()
-            }else{
+            } else {
                 Toast.makeText(applicationContext, it.message(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.loggedUser.observe(this) {
+            if (it) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
             }
         }
     }
 
-    private fun handleLogin(){
+    private fun handleLogin() {
         val email = binding.editEmail.text.toString()
         val password = binding.editPassword.text.toString()
 
--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -22,6 +22,11 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     private val _login = MutableLiveData<ValidationResult>()
     val login: LiveData<ValidationResult> = _login
 
+
+    private val _loggedUser = MutableLiveData<Boolean>()
+    val loggedUser: LiveData<Boolean> = _loggedUser
+
+
     /**
      * Faz login usando API
      */
@@ -48,6 +53,12 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
      * Verifica se usuário está logado
      */
     fun verifyLoggedUser() {
+        val token = securityPreferences.get(TaskConstants.SHARED.TOKEN_KEY)
+        val person = securityPreferences.get(TaskConstants.SHARED.PERSON_KEY)
+
+        RetrofitClient.addHeaders(token, person)
+
+        _loggedUser.value = (token != "" && person != "")
     }
 
 }
